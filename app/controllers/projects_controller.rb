1d2d21595914be922ad861f7f68da0b1169cb7f7
Charles Lai <charlesangeloplai@gmail.com>
2021-01-06 23:50:46 -0500

    update projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,33 @@
 class ProjectsController < ApplicationController
+  get '/projects' do
+    @projects = current_user.projects
+    erb :'/projects/list'
+  end
+  get '/projects/new' do
+    erb :'/projects/new'
+  end
+
+  post '/projects' do
+    project = Project.new(params[:project])
+    if project.save
+      redirect '/projects'
+    else
+      redirect '/projects/new'
+    end
+  end
+
+  get '/projects/:id' do
+    find_project
+    redirect_if_project_not_found
+    erb :'/projects/show'
+  end
+
+  private
+  def find_project
+    @project = Project.find_by_id(params[:id])
+  end
+
+  def redirect_if_project_not_found
+    redirect "/projects" unless @project
+  end
 end
