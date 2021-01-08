d5a418daf9b7b64490d230c3802be5aaca4b98d8
Charles Lai <charlesangeloplai@gmail.com>
2021-01-08 15:16:19 -0500

    update users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   has_many :projects
-  has_many :survey_data, through: :projects
 
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :cohort_id, presence: true
+  
   has_secure_password
 end
