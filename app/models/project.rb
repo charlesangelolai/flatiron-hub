388682062dbac1fb7278afa87e9f0af421d7dc88
Charles Lai <charlesangeloplai@gmail.com>
2021-01-08 15:18:20 -0500

    update project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,21 @@
 class Project < ActiveRecord::Base
-  has_many :surveys
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :phase_num, presence: true
+  validates :website_link, presence: true
+  validates :github_link, presence: true
+  validates :blog_link, presence: true
+  validates :video_link, presence: true
+  validates :user_id, presence: true
 end
+
+# t.string :name
+# t.string :description
+# t.integer :phase_num
+# t.string :website_link
+# t.string :github_link
+# t.string :blog_link
+# t.string :video_link
+# t.integer :user_id
